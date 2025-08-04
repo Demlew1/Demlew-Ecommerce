0b989c39d5c1df40fea9fd16f1f177b1ca278181
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-08-04 11:10:47 +0300

    Fixed the social icon links

--- src/components/Home/SocialLinks.tsx
+++ src/components/Home/SocialLinks.tsx
@@ -1,30 +1,30 @@
-import { FaGithub } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { FaTelegram } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
+import { FaGithub, FaTelegram, FaPhone } from "react-icons/fa";
 import type { SocialLinks } from "../../types/socialLinks";
+
 const socialLinks: SocialLinks[] = [
   {
     id: 1,
-    url: "https://www.facebook.com",
-    icon: <FaGithub className="size-4 duration-200 text-gray-100 " />,
+    url: "https://github.com/demlew1",
+    icon: (
+      <FaGithub className="size-4 duration-200 text-gray-100 hover:text-cyan-400" />
+    ),
   },
   {
     id: 2,
-    url: "https://x.com",
-    icon: <FaLinkedin className="size-4 duration-200 text-gray-100" />,
+    url: "https://t.me/sevsni",
+    icon: (
+      <FaTelegram className="size-4 duration-200 text-gray-100 hover:text-cyan-400" />
+    ),
   },
   {
     id: 3,
-    url: "https://youtube.com",
-    icon: <FaTelegram className="size-4 duration-200 text-gray-100 " />,
-  },
-  {
-    id: 4,
-    url: "https://www.instagram.com",
-    icon: <FaInstagram className="size-4 duration-200 text-gray-100 " />,
+    url: "tel:+251983334785",
+    icon: (
+      <FaPhone className="size-4 duration-200 text-gray-100 hover:text-cyan-400" />
+    ),
   },
 ];
+
 export default function SocialLinks() {
   return (
     <ul className="flex flex-row space-x-4">
@@ -32,7 +32,12 @@ export default function SocialLinks() {
         const { id, url, icon } = link;
         return (
           <li key={id}>
-            <a href={url} target="_blank" rel="noopener noreferrer">
+            <a
+              href={url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:scale-110 transition-transform"
+            >
               {icon}
             </a>
           </li>
